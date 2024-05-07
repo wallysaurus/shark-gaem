c0b38fb766dfd64f5e21111ea8e2f55a9599f06a
wally <69739715+wallysaurus@users.noreply.github.com>
2024-05-06 21:24:39 -0500

    isaac nuts

--- GodotProject/PlayerMovement.gd
+++ GodotProject/PlayerMovement.gd
@@ -2,7 +2,6 @@ extends RigidBody3D
 
 @export var underwater_speed = 14
 @export var fall_acceleration = 75
-@onready var pcam: PhantomCamera3D = get_node("../PhantomCamera3D2")
 @export var mouse_sensitivity: float = 0.5
 
 var min_yaw: float = -89.9
@@ -19,6 +18,7 @@ func _physics_process(delta):
 	
 	if (Input.is_key_pressed(KEY_D)):
 		direction.x += 1
+		
 	if (Input.is_key_pressed(KEY_A)):
 		direction.x -= 1
 	if (Input.is_key_pressed(KEY_S)):
@@ -36,8 +36,6 @@ func _physics_process(delta):
 	#if not is_on_floor():
 	#target_velocity.y = target_velocity.y + (fall_acceleration * delta)
 	
-	self.add_constant_force(target_velocity)
-	
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -46,15 +44,3 @@ func _input(event):
 		and event.button_index == MOUSE_BUTTON_LEFT \
 		and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-func _unhandled_input(event) -> void:
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		var pcam_rotation_degrees: Vector3
-
-		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
-		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_yaw, max_yaw)
-		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
-		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_pitch, max_pitch)
-		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
--- GodotProject/TerrainScript.gd
+++ GodotProject/TerrainScript.gd
@@ -1,42 +1,79 @@
 @tool
 extends StaticBody3D
 
+@export var boobs: bool = false:
+	get:
+		return boobs
+	set(new_boobs):
+		boobs = new_boobs
+		generate_mesh()
 @export var size = 64
 @export var subdivide = 63
 @export var amplitude = 16
 @export var noise = FastNoiseLite.new()
 @export var adjustment: float = 0.1
+@export var interpolation_speed = 0.05
+
+var current_vertices = []
+var target_vertices = []
+var mesh_initialized = false
 
 func generate_mesh():
 	print('actually generating')
 	
+	# Create our base plane mesh.
 	var plane_mesh = PlaneMesh.new()
 	plane_mesh.size = Vector2(size, size)
 	plane_mesh.subdivide_depth = subdivide
 	plane_mesh.subdivide_width = subdivide
 	
+	# Surface tool allows us to findiddle with the mesh vertices
+	# Data is a matrix of our vertices.
 	var surface_tool = SurfaceTool.new()
 	surface_tool.create_from(plane_mesh, 0)
 	var data = surface_tool.commit_to_arrays()
-	var vertices = data[ArrayMesh.ARRAY_VERTEX]
+	target_vertices = data[ArrayMesh.ARRAY_VERTEX]
 	
-	for i in vertices.size():
-		var vertex = vertices[i]
-		vertices[i].y = noise.get_noise_2d(vertex.x + adjustment, vertex.z) * amplitude
-	data[ArrayMesh.ARRAY_VERTEX] = vertices
+	# Loop through all the vertices and adjust their y value based on noisemap.
+	for i in target_vertices.size():
+		var vertex = target_vertices[i]
+		target_vertices[i].y = noise.get_noise_2d(vertex.x, vertex.z) * amplitude
+	if not mesh_initialized:
+		current_vertices = target_vertices.duplicate()
+		mesh_initialized = true
+	
+	update_mesh(data)
+
+func update_mesh(data):
+	data[ArrayMesh.ARRAY_VERTEX] = current_vertices
 	
 	var array_mesh = ArrayMesh.new()
 	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, data)
 	
+	var surface_tool = SurfaceTool.new()
 	surface_tool.create_from(array_mesh, 0)
 	surface_tool.generate_normals()
-
+	
 	$Terrain.mesh = surface_tool.commit()
 	$CollisionShape3D.shape = array_mesh.create_trimesh_shape()
 	
 func _ready():
-	if not Engine.is_editor_hint():
-		generate_mesh()
+	generate_mesh()
+	current_vertices = target_vertices.duplicate()
+	# for some reason lerping needs this i dunno
+	set_process(true)
+
+func _process(delta):
+	var change = false
+	for i in range(current_vertices.size()):
+		var current_y = current_vertices[i].y
+		var target_y = target_vertices[i].y
+		if current_y != target_y:
+			current_vertices[i].y = lerp(current_y, target_y, interpolation_speed * delta)
+			change = true
+	if change:
+		var data = $Terrain.mesh.surface_get_arrays(0)[Mesh.ARRAY_VERTEX]
+		update_mesh(data)
 
 func _on_timer_timeout():
 	adjustment += 1
